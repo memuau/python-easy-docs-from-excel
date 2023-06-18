e54faebdd1b0050de5a46f17fcb3f6b03eaeaa60
memuau <88461193+memuau@users.noreply.github.com>
2023-06-18 17:17:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from easy_docs_from_excel.TemplateFiller import TemplateFiller
 
-tf = TemplateFiller.from_yaml('myconfig.yml')
+tf = TemplateFiller.from_yaml('config_to_fill.yml') # Remember to change the name of the file if needed
 tf.make_all_docs()
